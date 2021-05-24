2997f811cbedfb68c6a1ab67385dd8cac038ae6c
LidiaStrelets <72388403+LidiaStrelets@users.noreply.github.com>
2021-05-24 15:01:13 +0000

    Deploying to gh-pages from @ LidiaStrelets/parcel-practice@e6f0b096940bc9367b4f351f2cc747589976015e 🚀

--- src.31b2519b.js
+++ src.31b2519b.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
+},{}],"Q42x":[function(require,module,exports) {
+const t="http://localhost:3000",n={title:"Алиса в стране чудес",author:"Льюис Кэрролл",genres:["фентези"],raiting:7.1},o={title:"Гарри Поттер",author:"Джоанн Роулинг",genres:["фентези"],raiting:10};async function a(n){const o={method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify(n)},a=await fetch(`${t}/books`,o);return await a.json()}a(n);
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss"),console.log("hello");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-practice/src.c097aae7.js.map
+"use strict";require("./sass/main.scss"),require("./js/post.js");
+},{"./sass/main.scss":"clu1","./js/post.js":"Q42x"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-practice/src.31b2519b.js.map
